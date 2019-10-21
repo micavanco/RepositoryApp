044469d7d9a8fe9baf175fcb079233662adaff64
micavanco <micavanco@gmail.com>
2019-10-21 13:56:10 +0200

    First commit - boilerplate project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainView from "./views/MainView";
+
+import "./scss/views/appView.scss";
+
+function App() {
+  return (
+      <main className="main-container">
+        <MainView/>
+      </main>
+  );
+}
+
+export default App;
